fffeb510bccda9b6aae76500c5b37170bf3dbf38
shaliniche-code <shalini.che@gmail.com>
2026-03-24 17:19:21 +0000

    server.js changed

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from Jenkins CI/CD 🚀");
+  res.send("Webhook 🚀");
 });
 
 app.listen(3000, "0.0.0.0", () => {
