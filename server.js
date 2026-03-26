99000dc6dc43944cad51cb3bd1a3b578e9f66bd7
shaliniche-code <shalini.che@gmail.com>
2026-03-26 11:46:54 +0000

    server.js modified

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("EC2 auto deploy 🚀");
+  res.send("EC2- auto-deploy 🚀");
 });
 
 app.listen(5000, "0.0.0.0", () => {
